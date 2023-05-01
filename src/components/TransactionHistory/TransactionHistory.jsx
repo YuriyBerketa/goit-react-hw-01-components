97af663d9c6fd0261b24dcdf7c79f07440154849
YuriyBerketa <115158490+YuriyBerketa@users.noreply.github.com>
2023-05-01 19:28:04 +0300

    vse

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,10 +1,10 @@
 import PropTypes from "prop-types";
 import { TransactionItem } from "./TransactionItem";
-
+import css from "./TransactionHistory.module.css";
 
 export const TransactionHistory = ({items}) => {
   return (
-      <table className="transaction-history">
+      <table className={css.transactionHistory}>
   <thead>
     <tr>
       <th>Type</th>
